47887702e4cd888945e0f660a3b2309c28ca14e7
riannylinet-web <riannylinet@gmail.com>
2026-05-03 11:29:51 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botName: "Ayora Bot",
+  botOwner: 'PROGRAMMER AYORA', // Your name
   ownerNumber: '254112318201', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
